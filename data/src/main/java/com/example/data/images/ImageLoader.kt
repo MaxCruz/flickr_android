5d40a9be128354b7827e64c803bfb3a0eda63da2
Max Cruz <maxc@blanclabs.com>
2017-05-21 11:28:43 -0500

    [Data] Image loader

--- data/src/main/java/com/example/data/images/ImageLoader.kt
+++ data/src/main/java/com/example/data/images/ImageLoader.kt
@@ -2,6 +2,9 @@ package com.example.data.images
 
 import android.widget.ImageView
 
+/**
+ * Interface to load images in a view
+ */
 interface ImageLoader {
 
     fun load(image: ImageView, url: String, placeholder: Int? = null)
--- data/src/main/java/com/example/data/images/ImageLoaderGlide.kt
+++ data/src/main/java/com/example/data/images/ImageLoaderGlide.kt
@@ -5,6 +5,9 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 
+/**
+ * Implementation for the image loader using Glide lib
+ */
 class ImageLoaderGlide(context: Context) : ImageLoader {
 
     val requestManager: RequestManager = Glide.with(context)
