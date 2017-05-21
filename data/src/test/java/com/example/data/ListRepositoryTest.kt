e34a27b634b508390ec578f0dce255b3739c6c5b
Max Cruz <maxc@blanclabs.com>
2017-05-21 11:05:28 -0500

    [Data] Unit test for list repository

---
+++ data/src/test/java/com/example/data/ListRepositoryTest.kt
@@ -0,0 +1,45 @@
+package com.example.data
+
+import com.example.data.dto.RequestPhotoInfoDTO
+import com.example.data.dto.RequestPhotosDTO
+import com.example.data.dto.RequestSizesDTO
+import com.example.data.dto.StatusDTO
+import com.example.data.remote.FlickrService
+import com.example.data.remote.ServiceFactory
+import com.example.data.repository.ListRepository
+import com.example.domain.models.Photo
+import org.junit.Before
+import org.junit.Test
+
+
+/**
+ * Unit test for the list repository
+ */
+class ListRepositoryTest {
+
+    lateinit var repository: ListRepository
+
+    @Before
+    fun setUp() {
+        val service = ServiceFactory.createService(FlickrService::class.java,
+                FlickrService.ENDPOINT)
+        repository = ListRepository(service)
+    }
+
+    @Test
+    fun shouldGetRecentPhotos() {
+        val observer = repository.getRecentPhotos(1, 1).test()
+        observer.awaitTerminalEvent()
+        observer.assertNoErrors()
+        observer.assertValue { it is Photo }
+    }
+
+    @Test
+    fun shouldSearchPhotos() {
+        val observer = repository.searchPhotos(1, 1, "guitar").test()
+        observer.awaitTerminalEvent()
+        observer.assertNoErrors()
+        observer.assertValue { it is Photo }
+    }
+
+}
