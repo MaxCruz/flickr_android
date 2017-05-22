5eeae0b528dec9d6e7e5976a90888df4ebcabbab
Max Cruz <maxc@blanclabs.com>
2017-05-22 15:37:00 -0500

    [Presentation] Fix reload bug

--- app/src/main/java/com/example/flickr/list/listener/EndlessRecyclerViewScrollListener.kt
+++ app/src/main/java/com/example/flickr/list/listener/EndlessRecyclerViewScrollListener.kt
@@ -9,7 +9,7 @@ abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener
 
     // The minimum amount of items to have below your current scroll position
     // before loading more.
-    private var visibleThreshold = 6
+    private var visibleThreshold = 2
     // The current offset index of data you have loaded
     private var currentPage = 0
     // The total number of items in the dataset after the last load
