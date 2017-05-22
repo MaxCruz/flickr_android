90ba1f272c9a76fc68d3f15cf957438f0ea3b524
Max Cruz <maxc@blanclabs.com>
2017-05-22 14:46:58 -0500

    [Data] Load local data when fails remote in list repository

--- data/src/main/java/com/example/data/repository/ListRepository.kt
+++ data/src/main/java/com/example/data/repository/ListRepository.kt
@@ -1,7 +1,9 @@
 package com.example.data.repository
 
+import com.example.data.dao.PhotoDAO
 import com.example.data.dto.RequestPhotosDTO
 import com.example.data.dto.StatusDTO
+import com.example.data.local.LocalStorage
 import com.example.data.remote.FlickrService
 import com.example.domain.exceptions.RequestErrorException
 import com.example.domain.models.Photo
@@ -9,10 +11,12 @@ import com.example.domain.repository.ListContractModel
 import io.reactivex.Observable
 import io.reactivex.Single
 
+
 /**
  * Implementation for the image list repository
  */
-class ListRepository(val flickrService: FlickrService) : ListContractModel {
+class ListRepository(val flickrService: FlickrService, val localStorage: LocalStorage) :
+        ListContractModel {
 
     override fun getRecentPhotos(perPage: Int, page: Int): Single<List<Photo>> {
         return getObservablePhoto(flickrService.getRecent(perPage, page))
@@ -20,6 +24,19 @@ class ListRepository(val flickrService: FlickrService) : ListContractModel {
 
     override fun searchPhotos(perPage: Int, page: Int, text: String): Single<List<Photo>> {
         return getObservablePhoto(flickrService.search(perPage, page, text))
+    }
+
+    override fun saveToLocalStorage(list: List<Photo>) {
+        val daoList = list.map(::PhotoDAO)
+        localStorage.saveObjectList(PhotoDAO::class.java, daoList)
+    }
+
+    override fun clearLocalStorage() {
+        localStorage.deleteAllObjects(PhotoDAO::class.java)
+    }
+
+    override fun getLocalEntries(): List<Photo> {
+        return localStorage.retrieveAllObjects(PhotoDAO::class.java).map(PhotoDAO::toPhoto)
     }
 
     private fun getObservablePhoto(single: Single<RequestPhotosDTO>): Single<List<Photo>> {
